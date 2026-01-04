c791274b2c30dd31ab441791c2532fbe1112b750
LeRaxs <93239343+ler4xs@users.noreply.github.com>
2026-01-04 16:18:13 +0700

    Delete js/main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// scroll highlight (ง่าย ๆ)
-const links = document.querySelectorAll(".sidebar a");
-
-links.forEach(link => {
-  link.addEventListener("click", () => {
-    links.forEach(l => l.classList.remove("active"));
-    link.classList.add("active");
-  });
-});
